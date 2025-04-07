51c1224f35850b26055e77c45be0745c61bea4e2
ArchLeaders <archleadership28@gmail.com>
2025-04-06 20:14:25 -0700

    Revert "Ooooh so that's why (v2)"
    This reverts commit 29f0662066394464caa9c05d92f12f442836c336.

--- Extensions/TkSharp.Extensions.LibHac/Util/TkNandUtils.cs
+++ Extensions/TkSharp.Extensions.LibHac/Util/TkNandUtils.cs
@@ -29,7 +29,7 @@ public static class TkNandUtils
         LayeredFileSystem fs = new(sources);
 
         SwitchFs switchFs = SwitchFs.OpenNcaDirectory(keys, fs);
-        TkGameRomUtils.IsValid(switchFs, out hasUpdate);
+        bool result = TkGameRomUtils.IsValid(switchFs, out hasUpdate);
 
         if (switchFsContainer is not null) {
             foreach (IFileSystem disposable in sources) {
@@ -37,7 +37,7 @@ public static class TkNandUtils
             }
             
             switchFsContainer.Add((nandFolderPath, switchFs));
-            return hasUpdate;
+            return result;
         }
 
         foreach (IFileSystem disposable in sources) {
@@ -46,6 +46,6 @@ public static class TkNandUtils
         
         fs.Dispose();
         switchFs.Dispose();
-        return hasUpdate;
+        return result;
     }
 }
