598d996b93aeaddb9863b1dc730a275b952d8017
ArchLeaders <archleadership28@gmail.com>
2025-07-25 10:09:38 -0700

    Comment out auto data updater

--- TkSharp/TkSystemUpdater.cs
+++ TkSharp/TkSystemUpdater.cs
@@ -1,19 +1,13 @@
-using TkSharp.Core;
-using TkSharp.Core.Models;
-
 namespace TkSharp;
 
 public static class TkSystemUpdater
 {
     public static bool UpdateToData2(string dataFolder, string legacyDataFolder)
     {
-        var legacy = TkModManager.CreateLegacy(dataFolder, legacyDataFolder, TkSystemVersion.V1);
+        // var legacy = TkModManager.CreateLegacy(dataFolder, legacyDataFolder, TkSystemVersion.V1);
 
-        // Upgrade
-        
-        foreach (TkMod mod in legacy.Mods) {
-            
-        }
+        // Unused for this upgrade,
+        // but may be useful later.
 
         return false;
     }
