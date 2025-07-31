9cc83654bb2535ba7c63194236e0ec8a9368544a
LordBubbles <48891568+LordBubblesDev@users.noreply.github.com>
2025-07-31 20:18:47 +0200

    oops 22

--- Extensions/TkSharp.Extensions.GameBanana/Helpers/InternetHelper.cs
+++ Extensions/TkSharp.Extensions.GameBanana/Helpers/InternetHelper.cs
@@ -4,5 +4,14 @@ namespace TkSharp.Extensions.GameBanana.Helpers;
 
 public static class InternetHelper
 {
-    public static bool HasInternet => new Ping().Send("107.180.58.59", 1000).Status == IPStatus.Success;
+    public static bool HasInternet {
+        get {
+            try {
+                return new Ping().Send("107.180.58.59", 1000)?.Status == IPStatus.Success;
+            }
+            catch {
+                return false;
+            }
+        }
+    }
 }
