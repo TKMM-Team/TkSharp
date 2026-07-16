d5776f0d17f2652154af8105a305eba9b04b0dab
LordBubbles <48891568+LordBubblesDev@users.noreply.github.com>
2026-07-16 14:22:01 +0200

    GB feed metadata: add TotalPageCount int

--- Extensions/TkSharp.Extensions.GameBanana/GameBananaMetadata.cs
+++ Extensions/TkSharp.Extensions.GameBanana/GameBananaMetadata.cs
@@ -4,6 +4,20 @@ namespace TkSharp.Extensions.GameBanana;
 
 public class GameBananaMetadata
 {
+    [JsonPropertyName("_nRecordCount")]
+    public int RecordCount { get; set; }
+
     [JsonPropertyName("_bIsComplete")]
     public bool IsCompleted { get; set; }
+
+    [JsonPropertyName("_nPerpage")]
+    public int PerPage { get; set; }
+
+    /// <summary>
+    /// Total number of pages for this feed. One page when there are at most <see cref="PerPage"/> records.
+    /// </summary>
+    [JsonIgnore]
+    public int TotalPageCount => PerPage > 0 && RecordCount > PerPage
+        ? (int)Math.Ceiling(RecordCount / (double)PerPage)
+        : 1;
 }
