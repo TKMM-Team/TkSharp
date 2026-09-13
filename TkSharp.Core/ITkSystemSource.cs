deab3269380908d55286a724dce814928b8e032c
Aiden A <76793340+Mindstormman06@users.noreply.github.com>
2026-09-13 14:00:11 -0700

    Remove EnumerateFiles method

--- TkSharp.Core/ITkSystemSource.cs
+++ TkSharp.Core/ITkSystemSource.cs
@@ -12,6 +12,4 @@ public interface ITkSystemSource
     ITkSystemSource GetRelative(string relativeSourcePath);
 
     IEnumerable<string> EnumerateRomfsBuckets() => [];
-
-    IEnumerable<string> EnumerateFiles(string relativeFolderPath) => [];
 }
